a69dc2532659505881fa710458922c6471b672fb
Dmytro Baturin <dbaturin02@gmail.com>
2024-11-12 00:01:49 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["*", "s3-alpha-sig.figma.com"],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
